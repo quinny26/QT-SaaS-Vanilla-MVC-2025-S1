85ee05423bc4a92dbf8bd6a9c2f07af95c990f87
quinny26 <quinnietrang823@gmail.com>
2025-03-26 01:18:41 +0800

    wip: Commence Framework
    - Create the db.php config file
    - Create helper.php file with functions

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Database Configuration File
+ *
+ * Filename:        db.php
+ * Location:        config/
+ * Project:         qt-SaaS-Vanilla-MVC-S1
+ * Date Created:    DATE_CREATED
+ *
+ * Author:          Quinny
+ *
+ */
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'qt_saas_vanilla_mvc',
+    'username' => 'qt_saas_vanilla_mvc',
+    'password' => 'Password1234'
+];
